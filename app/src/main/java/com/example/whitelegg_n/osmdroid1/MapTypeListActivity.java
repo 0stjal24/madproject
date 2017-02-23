94ec2ff03b941d6cf2feeed49098e34074ad1591
0stjal24 <0stjal24@solent.ac.uk>
2017-02-23 12:32:44 +0000

    Custom lists

--- app/src/main/java/com/example/whitelegg_n/osmdroid1/MapTypeListActivity.java
+++ app/src/main/java/com/example/whitelegg_n/osmdroid1/MapTypeListActivity.java
@@ -1,23 +1,30 @@
 package com.example.whitelegg_n.osmdroid1;
 
 import android.app.ListActivity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 
 public class MapTypeListActivity extends ListActivity
 {
-    String[] data;
+    private String[] names;
+    private String[] descriptions;
 
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        data = new String[] { "Regular Map", "Cycle Map"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, data);
-        setListAdapter(adapter);
+        names = new String[] { "Regular Map", "Cycle Map"};
+        descriptions = new String[] { "Loads the regular map", "Show Cycle Map"};
+
+        MyAdapter mapAdapter = new MyAdapter();
+        setListAdapter(mapAdapter);
     }
 
     public void onListItemClick(ListView lv, View view, int index, long id) {
@@ -33,7 +40,7 @@ public class MapTypeListActivity extends ListActivity
         // Create an Intent to send information back to the main activity.
         Intent intent = new Intent();
 
-        // Create a Bundle to store data to be sent back.
+        // Create a Bundle to store names to be sent back.
         Bundle bundle = new Bundle();
 
         // Add the boolean to the Bundle.
@@ -48,6 +55,32 @@ public class MapTypeListActivity extends ListActivity
         // Close the second activity (this will return to the first)
         finish();
 
+    }
+
+    class MyAdapter extends ArrayAdapter<String> {
+
+        public MyAdapter() {
+            super(MapTypeListActivity.this, android.R.layout.simple_list_item_1, names);
+        }
+
+        @Override
+        public View getView(int index, View convertView, ViewGroup parent){
+            // inflate our poi entry layout
+            LayoutInflater inflater;
+            inflater = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+            View view = inflater.inflate(R.layout.poi_entry, parent, false);
+
+            //populate our poi entry with names
+            TextView nameTextView = (TextView) view.findViewById(R.id.poi_name);
+            nameTextView.setText(names[index]);
+
+            TextView descriptionTextView = (TextView) view.findViewById(R.id.poi_name);
+            descriptionTextView.setText(descriptions[index]);
+
+            // return view
+            return view;
+        }
     }
 
 
